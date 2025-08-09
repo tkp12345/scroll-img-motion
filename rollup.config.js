53f448162700ecddd1a4667934bfde8516acb2e6
robertttttttto <roberto@bhaptics.com>
2025-08-09 14:05:52 +0900

    feat: package init

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'auto'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    nodeResolve({
+      browser: true
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
+    terser()
+  ],
+  external: []
+};
