8774a2af544c54414166da1d4c9d958633dce9d1
tkp12345 <tkp1@hanmail.com>
2025-09-07 14:27:55 +0900

    feat: add useStableref.ts - utils hooks

---
+++ src/hooks/useStableref.ts
@@ -0,0 +1,7 @@
+import { useRef } from "react";
+
+export function useStableRef<T>(initial: T) {
+  const ref = useRef<T | null>(null);
+  if (ref.current === null) ref.current = initial;
+  return ref as React.MutableRefObject<T>;
+}
